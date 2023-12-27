9ee23b90801451af6592c6bb8b5dbf2d7faaf7a0
Sanjay <sbs13082003@gmail.com>
2023-12-27 17:38:50 +0530

    adding home page components

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Home from './Home'
+import Events from './Events'
+import Workshop from './Workshop'
+import Team from './Team'
+import Contact from './Contact'
+
+export {
+    Home, Events, Workshop, Team, Contact
+}
