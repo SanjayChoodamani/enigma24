2595c81b5bbae528b87915a8ed18a177996fce84
Arjun H L <arjunhl360@gmail.com>
2024-03-23 16:29:25 +0530

    Updated workshop and added margin to the nav

--- src/components/DisplayCard.jsx
+++ src/components/DisplayCard.jsx
@@ -1,56 +1,59 @@
-import React from 'react';
-import './styles/DisplayCard.css'
+import React from "react";
+import "./styles/DisplayCard.css";
 
 export default function DisplayCard(props) {
-    return (props.trigger) ? (
-        <div className='card'>
-            <div className='inner-card'>
-                <button onClick={() => props.setTrigger(false)} >
-                    <i className="fa-solid fa-xmark"></i>
-                </button>
-                <div className='event-display'>
-                    <div className='event-register'>
-                        <div className='details'>
-                            <p>Venue: </p>
-                            <h6>MCE</h6>
-                            <p>Date: </p>
-                            <h6>04/2024</h6>
-                            <div>
-                                <p>Contact: </p>
-                                <h6>{props.desc.phone}</h6>
-                            </div>
-                        </div>
-                        <div className='register-column'>
-                            <div>
-                                <p>Event Heads: </p>
-                                <h6>{props.desc.heads[0]}</h6>
-                                <h6>{props.desc.heads[1]}</h6>
-                            </div>
-                            <br/>
-                            <a href='#'>Register</a>
-                        </div>
-                    </div>
-                    <div className='displayCardDesc'>
-                        <h1>{props.desc.name}</h1>
-                        <h2>Description</h2>
-                        <p>{props.desc.desc}</p>
-                        {props.desc.details.map((detail) => {
-                            return (
-                                <>
-                                    <h2>{detail.heading}</h2>
-                                    {detail.desc.map((content) => {
-                                        return (
-                                            <p>{content}</p>
-                                        );
-                                    })}
-                                </>
-                            );
-                        })}
-                    </div>
-                </div>
+  console.log(props);
 
-
+  return props.trigger ? (
+    <div className="card">
+      <div className="inner-card">
+        <button onClick={() => props.setTrigger(false)}>
+          <i className="fa-solid fa-xmark"></i>
+        </button>
+        <div className="event-display">
+          <div className="event-register">
+            <div className="details">
+              <p>Venue: </p>
+              <h6>{props.desc.loc}</h6>
+              <p>Date: </p>
+              <h6>{props.desc.date}</h6>
+              <div>
+                <p>Contact: </p>
+                <h6>{props.desc.phone[0]}</h6>
+                <h6>{props.desc.phone[1]}</h6>
+              </div>
             </div>
+            <div className="register-column">
+              <div>
+                <p>Event Heads: </p>
+                <h6>{props.desc.heads[0]}</h6>
+                <h6>{props.desc.heads[1]}</h6>
+              </div>
+              <br />
+              {props.desc.register && (
+                <a href={props.desc.register}>Register</a>
+              )}
+            </div>
+          </div>
+          <div className="displayCardDesc">
+            <h1>{props.desc.name}</h1>
+            <h2>Description</h2>
+            <p>{props.desc.desc}</p>
+            {props.desc.details.map((detail) => {
+              return (
+                <>
+                  <h2>{detail.heading}</h2>
+                  {detail.desc.map((content) => {
+                    return <p>{content}</p>;
+                  })}
+                </>
+              );
+            })}
+          </div>
         </div>
-    ) : "";
+      </div>
+    </div>
+  ) : (
+    ""
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,46 +1,46 @@
-import { useState, useEffect } from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import './styles/Navbar.css';
-import Menu from './Menu';
+import { useState, useEffect } from "react";
+import { Link, NavLink } from "react-router-dom";
+import "./styles/Navbar.css";
+import Menu from "./Menu";
 
 const Navbar = () => {
+  const [windowSize, setWindowSize] = useState([
+    window.innerWidth,
+    window.innerHeight,
+  ]);
 
-    const [windowSize, setWindowSize] = useState([
-        window.innerWidth,
-        window.innerHeight,
-    ]);
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowSize([window.innerWidth, window.innerHeight]);
+    };
 
-    useEffect(() => {
-        const handleWindowResize = () => {
-            setWindowSize([window.innerWidth, window.innerHeight]);
-        };
+    window.addEventListener("resize", handleWindowResize);
 
-        window.addEventListener('resize', handleWindowResize);
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
 
-        return () => {
-            window.removeEventListener('resize', handleWindowResize);
-        };
-    }, []);
+  return (
+    <nav>
+      <header className="nav-style">
+        <div className="nav-logo">
+          <Link to="/">ENIGMA'24</Link>
+        </div>
+        {windowSize[0] < 480 ? (
+          <Menu />
+        ) : (
+          <div className="nav-icons">
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/events">Events</NavLink>
+            <NavLink to="/pronite">Workshop and Pronite</NavLink>
+            <NavLink to="/team">Team</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
+          </div>
+        )}
+      </header>
+    </nav>
+  );
+};
 
-    return (
-        <nav>
-            <header className='nav-style'>
-                <div className='nav-logo'>
-                    <Link to="/">
-                        ENIGMA'24
-                    </Link>
-                </div>
-                {windowSize[0]<480 ? <Menu/> : 
-                <div className='nav-icons'>
-                    <NavLink to='/'>Home</NavLink>
-                    <NavLink to='/events'>Events</NavLink>
-                    <NavLink to='/pronite'>Workshop and Pronite</NavLink>
-                    <NavLink to='/team'>Team</NavLink>
-                    <NavLink to='/contact'>Contact</NavLink>
-                </div>}
-            </header>
-        </nav>
-    )
-}
-
-export default Navbar
+export default Navbar;
